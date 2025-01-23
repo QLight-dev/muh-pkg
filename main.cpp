fefeb8b9fc1f494c88b1506d6075cc40db6a2b37
QLight-dev <nayazmuhammed094@gmail.com>
2025-01-23 14:49:17 +0400

    Update main.cpp
    Fixed main.cpp and optimized it

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdexcept>
+#include <cstring>  // for strcmp
 
-std::string command;
-int main(int argc,char* argv[]) {
-    if (argc == 1 && argv[1] == "install") {
-        std::cerr << "muh: no input files!";
-        std::cerr << "usage: muh install <package>";
-    } else if (argv[1] == "install" && argc == 2) {
-        #if (defined(__linux__))
-            system(("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/QLight-dev/muh-pkg/refs/heads/main/packages/" + std::string(argv[2]) + "linux/install.sh)\"").c_str());
+int main(int argc, char* argv[]) {
+    if (argc == 1 || strcmp(argv[1], "install") != 0) {
+        std::cerr << "muh: no input files!" << std::endl;
+        std::cerr << "Usage: muh install <package>" << std::endl;
+    } else if (argc == 3 && strcmp(argv[1], "install") == 0) {
+        #if defined(__linux__)
+            std::string command = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/QLight-dev/muh-pkg/refs/heads/main/packages/" + std::string(argv[2]) + "linux/install.sh)\"";
+            system(command.c_str());
         #endif
     }
 }
