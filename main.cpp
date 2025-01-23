bdb078dd9108514db24aa3f32ab941a53734ef83
QLight-dev <nayazmuhammed094@gmail.com>
2025-01-23 10:16:46 +0000

    updated sfml

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <cstdlib>
 #include <stdexcept>
 
 int main(int argc, char* argv[]) {
     if (argc == 1 && argv[1] == "install") {
         std::cerr << "muh: no input files!";
         std::cerr << "usage: muh install <package>";
+    } else if (argv[1] == "install" && argc == 2) {
+        #if (defined(__linux__))
+            system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/QLight-dev/muh-pkg/refs/heads/main/packages/" + argv[2] + "/install.sh");
+        #endif
     }
 }
---
+++ packages/sfml/install.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/QLight-dev/muh-pkg/refs/heads/main/packages/SFML/SFML-3.0.0.tar.gz
+tar -xvf SFML-3.0.0.tar.gz
